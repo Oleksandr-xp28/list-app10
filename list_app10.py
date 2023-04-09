1734f3980e20f87800817fbcbfc9441afbd45483
Fedor_xp28 <Fedor_xp28@student.itstep.org>
2023-04-09 23:09:08 +0300

    ex_3 task done

--- list_app10.py
+++ list_app10.py
@@ -1,2 +1,13 @@
 
 # HW-3: Fedor_xp28@student.itstep.org
+
+try:
+    word_list = []
+    words = []
+    for i in range(5):
+        words.append(input("Enter word: "))
+        sorted_words = sorted(words, key=lambda x: len(x))
+    print(sorted_words)
+
+except Exception as e:
+    print(e)
