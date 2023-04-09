89a7f2d479f55a0c9b20903fc0db335c908ca192
Fedor_xp28@student.itstep.org <Fedor_xp28@student.itstep.org>
2023-04-09 22:06:57 +0300

    ex_1 task done Fedor_xp28

--- list_app10.py
+++ list_app10.py
@@ -1,2 +1,19 @@
+# Завдання 1: Напишіть програму, яка створює список з n випадкових чисел 
+# та виводить кількість парних та непарних чисел у списку.
+# HW-3: Fedor_xp28@student.itstep.org 
 
-# HW-3: Fedor_xp28@student.itstep.org
+try:
+    import random
+    n = int(input('Enter n: '))
+    lst = [random.randint(0, 100)]
+    for i in range(n):
+        if i % 2 == 0:
+            print(i)
+            print('Even:')
+        else:
+            continue
+except Exception as e:
+    print(e)
+    print('Error')
+finally:
+    print('Good bye')
