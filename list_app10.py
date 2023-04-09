bcb8f7763b8f9f7ad03fd45a4b210129e00e47ee
Fedor_xp28@student.itstep.org <Fedor_xp28@student.itstep.org>
2023-04-09 22:32:57 +0300

    ex_2 task done

--- list_app10.py
+++ list_app10.py
@@ -1,2 +1,16 @@
+# Завдання 2: Реалізуйте функцію, яка приймає список чисел та повертає список з унікальними елементами, 
+# зберігаючи їх порядок.
 
 # HW-3: Fedor_xp28@student.itstep.org
+try:
+    list = []
+    u_list = []
+    for x in range(0, 5):
+        list.append(int(input('Enter list: ')))
+        for item in list:
+            if item not in u_list:
+                u_list.append(item)
+    print(u_list)
+except Exception as e:
+    print(e)
+    print("Erorr")
