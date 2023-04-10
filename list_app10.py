b2adb34d4fb4e47fb942be747e07c18baf0a3884
Fedor_xp28 <Fedor_xp28@student.itstep.org>
2023-04-10 22:54:30 +0300

    ex_4 task done

--- list_app10.py
+++ list_app10.py
@@ -1,2 +1,17 @@
 
 # HW-3: Fedor_xp28@student.itstep.org
+
+try:
+    import random
+    list1 = [random.randint(1, 10) for i in range(10)]
+    list2 = [random.randint(1, 10) for i in range(10)]
+
+    print(list1)
+    print(list2)
+    common_elements = list(set(list1).intersection(set(list2)))
+
+    print(common_elements)
+except Exception as e:
+    print(e)
+finally:
+    print("The End")
