8ec5df8bd4deaf5e6fe77841a0571b09e44a12ec
Fedor_xp28 <Fedor_xp28@student.itstep.org>
2023-04-11 12:37:09 +0300

    task 6 done

--- list_app10.py
+++ list_app10.py
@@ -1,2 +1,10 @@
 
 # HW-3: Fedor_xp28@student.itstep.org
+try:
+    squared_numbers = []
+    for num in range(0, 5):
+        num = int(input("Enter a number: "))
+        squared_numbers.append(num**2)
+    print(squared_numbers)
+except :
+    print("Error: Enter a number")
