f8f353ece43b7b63fb8ad30eefa1a7bce07a17f1
Fedor_xp28 <Fedor_xp28@student.itstep.org>
2023-04-11 11:47:30 +0300

    ex_5 task done

--- list_app10.py
+++ list_app10.py
@@ -1,2 +1,12 @@
 
 # HW-3: Fedor_xp28@student.itstep.org
+
+try:
+    positive_numbers = []
+    for num in range(0, 5):
+        num = int(input("Enter a number: "))
+        if num > 0:
+            positive_numbers.append(num)
+    print(positive_numbers)
+except Exception as e:
+    print("Error: ", e)
