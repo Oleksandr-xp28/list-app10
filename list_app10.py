538a528acd9c40aa4c181f839b077c8b6b060b2e
Fedor_xp28 <Fedor_xp28@student.itstep.org>
2023-04-11 16:22:08 +0300

    Task done

--- list_app10.py
+++ list_app10.py
@@ -1,2 +1,12 @@
 
 # HW-3: Fedor_xp28@student.itstep.org
+
+try:
+    string_list = ["apple", "banana", "orange", "grape", "pear"]
+    substring = "a"
+
+    result = [string for string in string_list if string.startswith(substring)]
+
+    print(result)
+except Exception as e:
+    print(e)
