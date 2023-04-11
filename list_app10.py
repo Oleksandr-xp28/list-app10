beaa5e4498b70600da5e06d4dafbe52036f60f40
Fedor_xp28 <Fedor_xp28@student.itstep.org>
2023-04-11 12:42:17 +0300

    task 6 done

--- list_app10.py
+++ list_app10.py
@@ -1,2 +1,15 @@
 
 # HW-3: Fedor_xp28@student.itstep.org
+
+try:
+    numbers = []
+    for i in range(5):
+        numbers.append(int(input("Enter number: ")))
+    min_index = numbers.index(min(numbers))
+    max_index = numbers.index(max(numbers))
+    
+    numbers[min_index], numbers[max_index] = numbers[max_index], numbers[min_index]
+    
+    print(numbers)
+except :
+    print("Error: Enter a number")
