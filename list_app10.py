7ea23f8990ca4599c05ac0cea73e642920d8effb
Fedor_xp28 <Fedor_xp28@student.itstep.org>
2023-04-11 16:18:05 +0300

    task done

--- list_app10.py
+++ list_app10.py
@@ -1,2 +1,17 @@
 # HW-3: Fedor_xp28@student.itstep.org
 
+try:
+    import random
+
+    numbers = [random.randint(0, 100) for _ in range(10)]
+
+    mean = sum(numbers) / len(numbers)
+
+    count = sum(1 for num in numbers if num > mean)
+
+    print("Numbers:", numbers)
+    print("Mean:", mean)
+    print("Number of items above mean:", count)
+
+except :
+    pass
